ffd3b5b6a4ef3b946a59092d7d5d1544e95b09d1
bhavBains <deepbains26@gmail.com>
2017-11-23 02:09:38 +0000

    added login and register ajax request, changes in index.ejs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,42 @@
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+$(() => {
+  $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+  });
 
+  $(".loginForm").on("submit", function(event){
+    event.preventDefault();
+
+    $.ajax({
+      url: '/api/users/login',
+      type: 'PUT',
+      data: $(event.target).serialize()
+    })
+    .done(function(response){
+      console.log(response);
+    })
+    .fail(function(jqXHR, textStatus) {
+      console.log(textStatus);
+    })
+  
+  })
+
+  $(".loginForm").on("submit", function(event){
+    event.preventDefault();
+
+    $.ajax({
+      url: '/api/users/register',
+      type: 'PUT',
+      data: $(event.target).serialize()
+    })
+    .done(function(response){
+      console.log(response);
+    })
+    .fail(function(jqXHR, textStatus) {
+      console.log(textStatus);
+    })
+  
+  })
+  
+});  
+  
